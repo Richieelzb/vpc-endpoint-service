678647b23ac94df70cca41fede952b67fc476924
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-11-08 16:44:23 +0200

    add4

--- ec2-private.tf
+++ ec2-private.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "ec2-vm-private" {
   key_name      = var.key-pair
   //user_data              = file("${path.module}/app-install.sh")
   subnet_id              = module.vpc1.private_subnets[0]
-  vpc_security_group_ids = [aws_security_group.private-sg-ec2.id]
+  vpc_security_group_ids = [aws_security_group.private-sg1-ec2.id]
   #count                  = 3
   tags = {
     Name = "baston-ec2-private"
@@ -19,7 +19,7 @@ resource "aws_instance" "ec2-private-1" {
   subnet_id            = module.vpc2.private_subnets[2]
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
-  vpc_security_group_ids = [aws_security_group.private-sg-ec2.id]
+  vpc_security_group_ids = [aws_security_group.private-sg2-ec2.id]
   count                  = 1
   tags = {
     Name = "ec2-private-1"
@@ -33,7 +33,7 @@ resource "aws_instance" "ec2-private-2" {
   subnet_id            = module.vpc2.private_subnets[3]
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
-  vpc_security_group_ids = [aws_security_group.private-sg-ec2.id]
+  vpc_security_group_ids = [aws_security_group.private-sg2-ec2.id]
   count                  = 1
   tags = {
     Name = "ec2-private-2"
--- security.tf
+++ security.tf
@@ -31,10 +31,43 @@ resource "aws_security_group" "public-sg-ec2" {
   }
 }
 
-resource "aws_security_group" "private-sg-ec2" {
+resource "aws_security_group" "private-sg1-ec2" {
   name        = "allow-pvt-sg-ec2"
   description = "allow-pvt-sg-ec2"
   vpc_id      = module.vpc1.vpc_id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "pvt-ec2-sg"
+  }
+}
+
+resource "aws_security_group" "private-sg2-ec2" {
+  name        = "allow-pvt-sg-ec2"
+  description = "allow-pvt-sg-ec2"
+  vpc_id      = module.vpc2.vpc_id
 
   ingress {
     description = "TLS from VPC"
