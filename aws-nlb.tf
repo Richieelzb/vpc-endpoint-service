477687775fc657c99d43612da8e2be325af6970b
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-11-08 12:49:57 +0200

    creating

--- aws-nlb.tf
+++ aws-nlb.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "internal_nlb" {
   internal           = true
   load_balancer_type = "network"
   security_groups    = [aws_security_group.private-sg-ec2.id]
-  subnets            = module.vpc.private_subnets[*]
+  subnets            = module.vpc2.private_subnets[*]
 }
 
 resource "aws_lb_target_group" "nlb_tg" {
@@ -26,12 +26,14 @@ resource "aws_lb_listener" "listener" {
 }
 
 resource "aws_lb_target_group_attachment" "web_attachment-1" {
+  count            = 1
   target_group_arn = aws_lb_target_group.nlb_tg.arn
   target_id        = aws_instance.ec2-private-1.id
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "web_attachment-2" {
+  count            = 1
   target_group_arn = aws_lb_target_group.nlb_tg.arn
   target_id        = aws_instance.ec2-private-2.id
   port             = 80
--- ec2-private.tf
+++ ec2-private.tf
@@ -1,3 +1,17 @@
+resource "aws_instance" "ec2-vm-master" {
+  ami = data.aws_ami.my-data-ami.id
+  //ami = "ami-0bc691261a82b32bc"
+  instance_type = var.instance-type-list[0]
+  key_name      = var.key-pair
+  //user_data              = file("${path.module}/app-install.sh")
+  subnet_id              = module.vpc1.private_subnets[0]
+  vpc_security_group_ids = [aws_security_group.private-sg-ec2.id]
+  #count                  = 3
+  tags = {
+    Name = "baston-ec2-private"
+  }
+}
+
 resource "aws_instance" "ec2-private-1" {
   ami                  = "ami-0c93cc426b63c7cf2"
   instance_type        = var.instance-type-list[0]
