515dfe2400f69fa291d166fb9b4527b7256199e1
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-11-09 17:03:58 +0200

    create v6

--- security.tf
+++ security.tf
@@ -53,9 +53,9 @@ resource "aws_security_group" "private-sg1-ec2" {
   }
 
   egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
     cidr_blocks = ["0.0.0.0/0"]
   }
 
