132d0350a507821cc7d45692752dcf75d633948c
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-11-09 16:09:23 +0200

    create v6

--- security.tf
+++ security.tf
@@ -113,6 +113,14 @@ resource "aws_security_group" "private-sg-endpoint" {
 
   ingress {
     description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
